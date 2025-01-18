fb6afce0d3334c93b13597f0904f78d42e7f2720
Dodik780 <bom78g@gmail.com>
2025-01-18 13:15:34 +0300

    Update pr1.cpp

--- pr1.cpp
+++ pr1.cpp
@@ -1,6 +1,33 @@
+#include <iostream>
 using namespace std;
 
 int main()
 {
-  return 0;
+	char* a = new char[120 * 60 + 1];
+	a[120 * 60] = '\0';
+
+	for (int y = 0; y < 60; y++)
+	{
+		for (int x = 0; x < 60; x++)
+		{
+			a[y * 120 + x] = ' ';
+		}
+	}
+	float xx, yy;
+	for (int y = 0; y < 60; y++)
+	{
+		for (int x = 0; x < 60; x++)
+		{
+			xx = float(x / 120 * 2 - 1);
+			yy = float(y / 60 * 2 - 1);
+
+			if (xx * xx + yy * yy < 0.5)
+			{
+				a[y * 120 + x] = '@';
+			}
+		}
+	}
+	cout<<a;
+
+	return 0;
 }
